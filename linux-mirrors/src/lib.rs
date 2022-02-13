8eaa09ff6300843957058047d24fb6e31b8b87dd
kawaki-san <dev@kanjala.com>
2022-02-13 21:03:51 +0200

    refactor: include_str! inplace of include_bytes!

--- linux-mirrors/src/lib.rs
+++ linux-mirrors/src/lib.rs
@@ -7,16 +7,13 @@ pub(crate) type Result<T> = std::result::Result<T, Box<dyn std::error::Error + S
 
 fn http2_client() -> hyper::Client<hyper_openssl::HttpsConnector<HttpConnector>> {
     let builder = hyper::client::Client::builder();
-
     let mut ssl_build = SslConnector::builder(SslMethod::tls()).expect("creating ssl connector");
     ssl_build
         .set_alpn_protos(b"\x02h2")
         .expect("setting alpn protocols");
     let mut http = HttpConnector::new();
     http.enforce_http(false);
-
     let https = hyper_openssl::HttpsConnector::with_connector(http, ssl_build)
         .expect("creating https connector");
-
     builder.build::<_, hyper::Body>(https)
 }
--- mirro-rs/src/main.rs
+++ mirro-rs/src/main.rs
@@ -1,14 +1,16 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mirrors = match linux_mirrors::archlinux::mirrors().await {
-        Ok(mirrors) => mirrors,
-        Err(e) => {
-            eprintln!("{e}");
-            let bytes = include_bytes!("../../assets/arch_mirrors.json");
-            let str = String::from_utf8_lossy(bytes);
-            serde_json::from_str(&str)?
-        }
-    };
-    println!("Hello, world! {}", mirrors.countries.len());
+    let (mirrors_tx, _mirrors_rx) = tokio::sync::mpsc::channel(16);
+    tokio::spawn(async move {
+        let mirrors = match linux_mirrors::archlinux::mirrors().await {
+            Ok(res) => res,
+            Err(e) => {
+                eprintln!("{e}");
+                let local_file = include_str!("../../assets/arch_mirrors.json");
+                serde_json::from_str(local_file).unwrap()
+            }
+        };
+        mirrors_tx.send(mirrors).await
+    });
     Ok(())
 }
